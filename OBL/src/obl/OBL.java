ce60c2c8a68f18e97c4824d748d08fc751274847
fbenedic92560 <fernando.benedictti.ort@gmail.com>
2020-11-18 05:26:00 -0300

    UI

--- OBL/src/obl/OBL.java
+++ OBL/src/obl/OBL.java
@@ -18,36 +18,18 @@ import javax.swing.JOptionPane;
  */
 public class OBL {
 
+    private static void displayMainUI() {
+        
+    }
     /**
      * @param args the command line arguments
      * @throws java.lang.Exception
      */
     public static void main(String[] args) throws Exception {
-        ObjectManager anObjectManager = new ObjectManager();
-        InstructionObject anInstructionObject = new InstructionObject();
-        Persistence aPersistence = new PersistenceFile();
-        
-        
-        String nameFile = "c:\\temp\\assi_example001.txt";
-        if (!aPersistence.openPersistence(nameFile)) {
-            JOptionPane.showMessageDialog(new JFrame(), nameFile + "Error opening ", "Dialog", JOptionPane.ERROR_MESSAGE);
-            System.exit(0);
-        }
-
-        String line;
-        Boolean lineValid;
-        while ((line = aPersistence.retrieveOneLine()) != null) {
-            lineValid = anInstructionObject.analizeInstruction(line);
-            System.out.println(line + lineValid.toString());
-        }
-        
-        aPersistence.closePersistence();
-        
-        anObjectManager.addObject(new Object("test", 1, new java.lang.Object()));
-        anObjectManager.addObject(new Object("test2", 0, new java.lang.Object()));
-        
-        System.out.println(anObjectManager.read("test"));
-        anObjectManager.write("test2", 0);
-        System.out.println(anObjectManager.read("test"));
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 }
