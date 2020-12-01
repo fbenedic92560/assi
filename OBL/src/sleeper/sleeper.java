b980c0ab8cea0b2f856caab2e873b72a4c47067c
fbenedic92560 <fernando.benedictti.ort@gmail.com>
2020-11-30 23:00:37 -0300

    Sleeper initial programming

---
+++ OBL/src/sleeper/sleeper.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sleeper;
+
+/**
+ *
+ * @author Usuario
+ */
+public class sleeper {
+    public void sleep(ModeTimeAdvancer modeTimeAdvancer) {
+        switch (modeTimeAdvancer) {
+            case EVEN:
+                break;
+            case ODD:
+                break;
+            case NEUTRAL:
+                break;
+        }
+    }
+}
