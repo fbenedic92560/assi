c863a307d3d6855be972f9c1a49f90ac050cc0ac
fbenedic92560 <fernando.benedictti.ort@gmail.com>
2020-12-02 10:36:25 -0300

    sleepMinutesToEven()

--- OBL/src/sleeper/sleeper.java
+++ OBL/src/sleeper/sleeper.java
@@ -5,19 +5,71 @@
  */
 package sleeper;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Usuario
  */
 public class sleeper {
-    public void sleep(ModeTimeAdvancer modeTimeAdvancer) {
+    private static final Integer secondsOfMinutes = 60;
+    
+    public void sleepMinutes(ModeTimeAdvancer modeTimeAdvancer) {
         switch (modeTimeAdvancer) {
             case EVEN:
+                sleepMinutesToEven();
                 break;
             case ODD:
+                sleepMinutesToOdd();
                 break;
             case NEUTRAL:
                 break;
         }
     }
+    
+    private void sleepMinutesToEven() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Integer minutes = localDateTime.getMinute();
+        Integer seconds = localDateTime.getSecond();
+        
+        Integer secondsToSleep;
+        
+        Integer moduleMinutes = minutes % 2;
+        if (moduleMinutes == 1) {
+            secondsToSleep = secondsOfMinutes - seconds;
+        } else {
+            secondsToSleep = secondsOfMinutes - seconds + secondsOfMinutes;
+        }
+        
+        try {
+            TimeUnit.SECONDS.sleep(secondsToSleep);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(sleeper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void sleepMinutesToOdd() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Integer minutes = localDateTime.getMinute();
+        Integer seconds = localDateTime.getSecond();
+        
+        Integer secondsToSleep;
+        
+        Integer moduleMinutes = minutes % 2;
+        if (moduleMinutes == 1) {
+            secondsToSleep = secondsOfMinutes - seconds;
+        } else {
+            secondsToSleep = secondsOfMinutes - seconds + secondsOfMinutes;
+        }
+        
+        try {
+            TimeUnit.SECONDS.sleep(secondsToSleep);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(sleeper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
