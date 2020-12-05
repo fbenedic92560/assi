190019adab3e63c2c600589ae92926e534f7be9f
fbenedic92560 <fernando.benedictti.ort@gmail.com>
2020-12-05 19:49:12 -0300

    Message - convertStringToBitArray() and tests

---
+++ OBL/src/message/Message.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package message;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Message {
+    
+    private static final Integer QUANTITY_BITS_OF_BYTE = 8;
+    private static final Character ZERO_CHARACTER = '0';
+    
+    private String addInitialQuantityOfCharacter(String binaryString, Character character, Integer quantityCharacters) {
+        String paddedBinaryString;
+        Character[] padArray = new Character[quantityCharacters];
+        
+        Arrays.fill(padArray, character);
+        paddedBinaryString = "";
+        for (Integer i = 0; i < quantityCharacters; i++) {
+            paddedBinaryString += padArray[i];
+        }
+        for (Integer i = 0; i < binaryString.length(); i++) {
+            paddedBinaryString += binaryString.charAt(i);
+        }
+        return paddedBinaryString;
+    }
+    
+    public String convertMessageToBitsArray(String message) {
+        Integer asciiValue, quantityInitialZeroCharacters;
+        String binaryPortionOfMessage, paddedBinaryPortionOfMessage;
+        String binaryMessage = "";
+        
+        for (char c : message.toCharArray()) {
+            asciiValue = (int) c;
+            binaryPortionOfMessage = Integer.toBinaryString(asciiValue);
+            quantityInitialZeroCharacters = QUANTITY_BITS_OF_BYTE - binaryPortionOfMessage.length();
+            
+            paddedBinaryPortionOfMessage = this.addInitialQuantityOfCharacter(binaryPortionOfMessage, ZERO_CHARACTER, quantityInitialZeroCharacters);
+            binaryMessage = binaryMessage + paddedBinaryPortionOfMessage;
+        }
+        
+        for (char c : binaryMessage.toCharArray()) {
+            System.out.print(c);
+        }
+        
+        return binaryMessage;
+    }    
+}
---
+++ OBL/test/message/MessageTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package message;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MessageTest {
+    
+    public MessageTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of convertMessageToBitsArray method, of class Message.
+     */
+    @Test
+    public void testConvertMessageOneCharacterToBitsArray() {
+        String message = "m";
+        Message instance = new Message();
+        String expResult = "01101101";
+        String result = instance.convertMessageToBitsArray(message);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testConvertMessageToBitsArray() {
+        String message = "mensaje";
+        Message instance = new Message();
+        String expResult = "01101101011001010110111001110011011000010110101001100101";
+        String result = instance.convertMessageToBitsArray(message);
+        assertEquals(expResult, result);
+    }    
+}
